dc5d4329cd9e945bdc41a168560e4055280efa61
Chinazam Dennar <exchangehub@aol.com>
2024-05-19 23:03:50 -0600

    first_commit

---
+++ infra/provider.tf
@@ -0,0 +1,30 @@
+terraform {
+    backend "remote" {
+      organization = "the_hub"
+      workspaces {
+        name = "terraform_workspace"
+      }
+    }
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.6.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 5.6.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("my-key.json")
+  project     = var.google_cloud_project_id
+  region      = "us-cental1"
+}
+
+provider "google-beta" {
+  credentials = file("my-key.json")
+  project     = var.google_cloud_project_id
+  region      = "us-cental1"
+}
