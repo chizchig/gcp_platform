ce7310712df93f1bb8968d3c3f2ab4cb9f842c7e
Chinazam Dennar <exchangehub@aol.com>
2024-05-20 19:36:08 -0600

    alpha commit

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
-    hostname     = "app.terraform.io/power_hub"
+    hostname     = "app.terraform.io"
     organization = "power_hub"  # Ensure this is the correct organization name
     workspaces {
       name = "gcp_platform"
